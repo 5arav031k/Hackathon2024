588a447651ec24dbc5b78da576ad669a1f962916
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-11-23 16:03:10 +0100

    feat: delete logic to save cvs locally

--- load_and_save.py
+++ load_and_save.py
@@ -1,16 +1,6 @@
 from flask import Flask, request
-import os
 
 app = Flask(__name__)
-
-UPLOAD_FOLDER = './datasets'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['ALLOWED_EXTENSIONS'] = {'csv', 'xlsx'}
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
-
 
 @app.route('/', methods=['POST'])
 def upload_page():
@@ -21,19 +11,6 @@ def upload_page():
     if file.filename == '':
         return 'No selected file'
 
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-
-    if file and allowed_file(file.filename):
-
-        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(filename)
-        return f'File successfully uploaded and saved as {filename}'
-    else:
-        return 'Invalid file format. Only CSV and Excel files are allowed.'
-
-
-# if __name__ == '__main__':
-#     if not os.path.exists(app.config['UPLOAD_FOLDER']):
-#         os.makedirs(app.config['UPLOAD_FOLDER'])
-#     app.run(debug=True)
+    if file:
+        ##yariks_method(file)
+        return f'File {file.filename} successfully added to yariks method'
