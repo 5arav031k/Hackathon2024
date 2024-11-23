9e08e18b5552b427c12a54b04025ee7f01602551
Vladyslav Boiko <vladyslav.boiko@student.tuke.sk>
2024-11-23 14:04:21 +0100

    first good idea

--- ai-front/src/App.js
+++ ai-front/src/App.js
@@ -2,9 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>hellow worldpiufdiabfdbfbj</p>
-    </div>
+      <div className="App">
+          <div className="container">
+              <div className="search-bar">
+                  <input type="text" placeholder="Value"/>
+                  <select>
+                      <option>Value</option>
+                      <option>Option 1</option>
+                      <option>Option 2</option>
+                  </select>
+                  <select>
+                      <option>Value</option>
+                      <option>Option 1</option>
+                      <option>Option 2</option>
+                  </select>
+                  <button>Button</button>
+              </div>
+              <div className="result-box"></div>
+          </div>
+      </div>
   );
 }
 
