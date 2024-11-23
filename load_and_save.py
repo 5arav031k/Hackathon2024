a19f15c2f0beada5eedaf6c06e71ec45bf8e6787
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-11-23 14:40:01 +0100

    perf: change page path

--- load_and_save.py
+++ load_and_save.py
@@ -12,7 +12,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 
-@app.route('/upload', methods=['POST'])
+@app.route('/', methods=['POST'])
 def upload_page():
     if 'dataset' not in request.files:
         return 'No file part'
