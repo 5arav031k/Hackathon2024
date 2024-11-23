0662178dfaf0c924fd04733e9eb7db4adcc4ca64
Vladyslav Boiko <vladyslav.boiko@student.tuke.sk>
2024-11-23 18:05:02 +0100

    beta version

--- ai-backend/app.py
+++ ai-backend/app.py
@@ -1,9 +1,40 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from os import makedirs, path
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def home():
-    return "Hellow"
+UPLOAD_FOLDER = './uploads'
+makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-app.run(debug=True)
+@app.route('/receive-data', methods=['POST'])
+def receive_data():
+    try:
+        data = request.json
+        print("Get data:", data)
+
+        # Возвращаем подтверждение клиенту
+        return jsonify({"message": "The data was received"}), 200
+    except Exception as e:
+        print("Error with data:", str(e))
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/upload-file', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({"error": "File not found in request"}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({"error": "File not selected"}), 400
+
+    # Сохраняем файл
+    file.save(path.join(UPLOAD_FOLDER, file.filename))
+    print(f"File uploaded successfully: {file.filename}")
+    return jsonify({"message": f"File {file.filename} successfully uploaded!"}), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- ai-front/src/App.js
+++ ai-front/src/App.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import DynamicSelect from './DynamicSelect'
+import ModuleSelect from './ModuleSelect';
+import DatasetSelect from './DatasetSelect';
+import axios from "axios";
 
 function App() {
+    const [inputValue, setInputValue] = useState("");
+
+    const handleSendData = async () => {
+        try {
+            const jsonData = {
+                value: inputValue,
+            };
+
+            await axios.post("http://127.0.0.1:5000/receive-data", jsonData, {
+                headers: { "Content-Type": "application/json" },
+            });
+
+            console.log("Данные успешно отправлены на сервер!");
+        } catch (error) {
+            console.error("Ошибка при отправке данных:", error);
+        }
+    };
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            handleSendData(); // Выполнить отправку данных при нажатии Enter
+        }
+    };
     return (
         <div className="App">
             <div className="container">
                 <div className="search-bar">
-                    <input type="text" placeholder="Value" />
+                    <input type="text"
+                           placeholder="Write your query here..."
+                           value={inputValue}
+                           onChange={(e) => setInputValue(e.target.value)}
+                           onKeyDown={handleKeyDown}
+                    />
 
-                    {/* First dynamic select */}
-                    <DynamicSelect apiUrl="https://api.example.com/options1" placeholder="Select Options 1" />
+                    <button onClick={handleSendData}>Submit</button>
 
-                    {/* Second dynamic select */}
-                    <DynamicSelect apiUrl="https://api.example.com/options2" placeholder="Select Options 2" />
+                    <DatasetSelect apiUrl="https://api.example.com/options1" placeholder="Choose dataset"/>
+                    <ModuleSelect apiUrl="https://api.example.com/options2" placeholder="Select  model"/>
 
-                    <button>Button</button>
+                    <input type="file"/>
                 </div>
                 <div className="result-box"></div>
             </div>
